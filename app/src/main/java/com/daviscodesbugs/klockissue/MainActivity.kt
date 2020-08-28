0d3c284d6955aad58fa52db2bb62d711a3815359
daviscodesbugs <daviscodesbugs@gmail.com>
2020-08-28 08:13:33 -0600

    Include Klock and attempt to use method

--- app/src/main/java/com/daviscodesbugs/klockissue/MainActivity.kt
+++ app/src/main/java/com/daviscodesbugs/klockissue/MainActivity.kt
@@ -2,10 +2,14 @@ package com.daviscodesbugs.klockissue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.soywiz.klock.DateTime
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val now = DateTime.now()
+        println("Now: $now")
     }
 }
